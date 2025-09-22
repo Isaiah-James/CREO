044f6d633e2b43b3afaf813c8485fdfa7eed08b4
Isaiah James <132225463+Isaiah-James@users.noreply.github.com>
2025-09-21 21:13:24 -0400

    Moved auth into standalone app

--- apps/creo-web-main/src/shared/features/auth/components/AuthForm.tsx
+++ apps/creo-web-main/src/shared/features/auth/components/AuthForm.tsx
@@ -2,7 +2,7 @@
 import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import RegisterForm from './RegisterForm';
-import LoginForm from './LoginForm';
+import LoginForm from '../../../../../../creo-web-auth/src/components/LoginForm';
 
 export default function AuthForm() {
     const params = useSearchParams();
